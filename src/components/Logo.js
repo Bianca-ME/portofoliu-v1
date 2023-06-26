14f88e5de89da1bd9d4fe2993fc74914c92fb523
b4a <74990094+Bianca-ME@users.noreply.github.com>
2023-06-26 20:30:42 +0300

    just commented out unused files

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 // assets
-import logoimg from '../assets/images/logo/bi.svg';
-import self_portrait1 from '../assets/images/self_portraits/me_b.jpg';
-import self_portrait1_crop1 from '../assets/images/self_portraits/me_b_crop1.jpg';
+// import logoimg from '../assets/images/logo/bi.svg';
+// import self_portrait1 from '../assets/images/self_portraits/me_b.jpg';
+// import self_portrait1_crop1 from '../assets/images/self_portraits/me_b_crop1.jpg';
 import self_portrait1_crop2 from '../assets/images/self_portraits/me_b_crop2.jpg';
 // import self_portrait1_crop3 from '../assets/images/self_portraits/me_b_crop3.jpg';
 
